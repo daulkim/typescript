e4bf25f769e1615ea1a5e0de034184e2ea445797
daulkim <kimdu0825@gmail.com>
2021-08-19 22:35:43 +0900

    Arrays and tuples

--- typescript-basics/hello-world.ts
+++ typescript-basics/hello-world.ts
@@ -1,10 +1,25 @@
 var a: number;
 var b: boolean;
 var c: string;
+var foo : undefined;
 
 a = 10;
 b = true;
 c = 'Hello';
 
+var myArr: number[];
+myArr = [];
+myArr = [1,2];
+myArr.push(1);
+
+a = myArr.pop();
 // compile error
 // a = true;
+// b = myArr.pop();
+
+/**
+ * javascript -> 여러 타입을 가지는 배열 가능
+ * typescript -> tuples 
+ **/
+var myTuple: [number,boolean];
+myTuple = [1,true];
