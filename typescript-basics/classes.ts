677ff0a37d754b021a3b30de5bc93b7abbf28900
daulkim <kimdu0825@gmail.com>
2021-08-23 22:49:01 +0900

    Methods and constructors

--- typescript-basics/classes.ts
+++ typescript-basics/classes.ts
@@ -1,8 +1,25 @@
 class Person {
     firstName : string;
     lastName : string;
+
+    // Multiple constructor implementations are not allowed.
+    // constructor(){
+    //     this.firstName = " ";
+    //     this.lastName = " ";
+    // }
+  
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+
+    getFullName() {
+        return this.firstName+ " " +this.lastName;
+    }
 }
 
-var personA = new Person();
-personA.firstName = "ken";
-console.log(personA.firstName);
+var personA = new Person("ken", "kim");
+
+console.log(personA.getFullName());
