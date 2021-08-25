10c8b1f5553f2d0a242158d0cf1a65b29fe18053
daulkim <kimdu0825@gmail.com>
2021-08-25 23:43:22 +0900

    Member visibility

--- typescript-basics/classes.ts
+++ typescript-basics/classes.ts
@@ -1,6 +1,6 @@
 class Person {
-    firstName : string;
-    lastName : string;
+    // private firstName : string;
+    // private lastName : string;
 
     // Multiple constructor implementations are not allowed.
     // constructor(){
@@ -8,15 +8,26 @@ class Person {
     //     this.lastName = " ";
     // }
   
-    constructor(firstName: string, lastName: string){
-        this.firstName = firstName;
-        this.lastName = lastName;
+    // 변수를 선언하지 않을수 있지만 이때는 꼭 접근제어자를 붙여줘야함
+    constructor(private firstName: string, private lastName: string){
+
     }
-
-
-
+    getFirstName(){
+        return this.firstName;
+    }
     getFullName() {
         return this.firstName+ " " +this.lastName;
+    }
+    setFirstName(firstName: string){
+        this.firstName = this.firstName;
+    }
+
+    getlastName(){
+        return this.lastName;
+    }
+
+    setlastName(lastName: string){
+        this.lastName = this.lastName;
     }
 }
 
