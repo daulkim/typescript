efb04cf965777ea70a2e671342ee061fc161a56d
daulkim <kimdu0825@gmail.com>
2021-09-01 00:26:06 +0900

    Calling service and troubleshooting errors

--- demo/src/GithubApiService.ts
+++ demo/src/GithubApiService.ts
@@ -2,9 +2,13 @@ import * as request from 'request';
 
 export class GithubApiService {
     getUserInfo(userName: string) {
-
-        request.get('https://api.github.com/users/'+userName, (response: any)=>{
-            console.log(response);
+        let options: any ={
+            headers: {
+                'User-Agent':'request'
+            }
+        }
+        request.get('https://api.github.com/users/'+userName, options, (response: any, body: any)=>{
+            console.log(body);
         });
     }
 
