40cad6f45af262972b7c3f680fec41919976d06c
daulkim <kimdu0825@gmail.com>
2021-09-05 00:36:25 +0900

    Using callbacks and handling repo response

--- demo/src/GithubApiService.ts
+++ demo/src/GithubApiService.ts
@@ -1,22 +1,28 @@
 import * as request from 'request';
+import { Repo } from './Repo';
 import { User } from './User';
 
+const OPTIONS: any ={
+    headers: {
+        'User-Agent':'request'
+    },
+    json: true
+};
+
 export class GithubApiService {
-    getUserInfo(userName: string) {
-        let options: any ={
-            headers: {
-                'User-Agent':'request'
-            },
-            json: true
-        }
-        request.get('https://api.github.com/users/'+userName, options, (error:any, response: any, body: any)=>{
+    getUserInfo(userName: string, cb: (user: User) => any) {
+
+        request.get('https://api.github.com/users/'+userName, OPTIONS, (error:any, response: any, body: any)=>{
             //let user = new User(JSON.parse(body))
             let user = new User(body)
-            console.log(user);
+            cb(user);
         });
     }
 
-    getRepost() {
-
+    getRepos(userName: string, cb: (repos: Repo[]) => any) {
+        request.get('https://api.github.com/users/'+userName+"/repos", OPTIONS, (error:any, response: any, body: any)=>{
+        let repos = body.map((repo: any) => new Repo(repo));
+        cb(repos);          
+        });
     }
 }
--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,4 +1,12 @@
 import { GithubApiService } from './GithubApiService';
+import { Repo } from './Repo';
+import { User } from './User';
 
 let svc = new GithubApiService();
-svc.getUserInfo('daulkim');
+svc.getUserInfo('daulkim', (user: User) => {
+    console.log(user);
+});
+
+svc.getRepos('daulkim', (repos: Repo[]) => {
+    console.log(repos);
+})
